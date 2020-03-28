b3ad0700320863dbe1dc87a9b07f73057207175e
Justi <Justi@Alpha-RX>
2020-03-27 22:20:32 -0400

    updated all prototypes

--- src/BSTDictionary.java
+++ src/BSTDictionary.java
@@ -7,7 +7,7 @@
  * @version March 27, 2020
  *
  */
-public class BSTDictionary {
+public class BSTDictionary<E, K extends Sortable> implements Dictionary {
 
 	/**
 	 * 
@@ -22,6 +22,36 @@ public class BSTDictionary {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
+	}
+
+	@Override
+	public Object search(Sortable key) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void insert(Sortable key, Object element) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void delete(Sortable key) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void printTree() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public int depth() {
+		// TODO Auto-generated method stub
+		return 0;
 	}
 
 }
